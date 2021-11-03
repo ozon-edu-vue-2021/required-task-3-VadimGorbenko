8267cc2be9e301cc433d6a1c4f8db2d8fc859ed4
Vadim Gorbenko <gamerlands@mail.ru>
2021-11-03 18:19:51 +0300

    #6 draw seats on the map with d3

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,7 +3,8 @@
     <h3>Карта офиса</h3>
 
     <div v-if="!isLoading" class="map-root">
-      <map-svg />
+      <map-svg ref="svg" />
+      <Table v-show="false" ref="table" />
     </div>
     <div v-else>Loading...</div>
   </div>
@@ -11,15 +12,68 @@
 
 <script>
 import MapSVG from "@/assets/images/map.svg";
+import * as d3 from "d3";
+import tables from "@/assets/data/tables.json";
+import Table from "@/assets/images/workPlace.svg";
+import legend from "@/assets/data/legend.json";
 
 export default {
   components: {
     "map-svg": MapSVG,
+    Table,
   },
   data() {
     return {
       isLoading: false,
+      svg: null,
+      g: null,
+      tableSVG: null,
+      tables: [],
     };
+  },
+  created() {
+    this.tables = tables;
+  },
+  mounted() {
+    this.isLoading = true;
+
+    this.svg = d3.select(this.$refs.svg);
+    this.g = this.svg.select("g");
+    this.tableSVG = d3.select(this.$refs.table);
+    if (this.g) {
+      this.drawTables();
+    } else {
+      alert("SVG is incorrect");
+    }
+
+    this.isLoading = false;
+  },
+  methods: {
+    drawTables() {
+      const svgTablesGroupPlace = this.g
+        .append("g")
+        .classed("groupPlaces", true);
+
+      this.tables.forEach((table) => {
+        const targetSeat = svgTablesGroupPlace
+          .append("g")
+          .attr("transform", `translate(${table.x}, ${table.y}) scale(0.5)`)
+          .attr("id", table._id)
+          .classed("employer-place", true);
+
+        targetSeat
+          .append("g")
+          .attr("transform", `rotate(${table.rotate || 0})`)
+          .attr("group_id", table.group_id)
+          .classed("table", true)
+          .html(this.tableSVG.html())
+          .attr(
+            "fill",
+            legend.find((it) => it.group_id === table.group_id)?.color ??
+              "transparent"
+          );
+      });
+    },
   },
 };
 </script>
